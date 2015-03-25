5c8cce74ccede77a3f6a37aceb2a9b1e109c0f49
haya14busa <hayabusa1419@gmail.com>
2015-03-25 18:53:42 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -s ~/dotfiles/.gitconfig .gitconfig
+ln -s ~/dotfiles/.gitignore .gitignore
+ln -s ~/dotfiles/.gvimrc .gvimrc
+ln -s ~/dotfiles/.inputrc .inputrc
+ln -s ~/dotfiles/.pythonstartup .pythonstartup
+ln -s ~/dotfiles/.tmux.conf .tmux.conf
+ln -s ~/dotfiles/.vimrc .vimrc
+ln -s ~/dotfiles/.zshrc .zshrc
