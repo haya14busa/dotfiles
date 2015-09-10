e1adc47025db45ead1286aa8c638f0eb5f459ec0
haya14busa <hayabusa1419@gmail.com>
2015-09-10 12:14:47 +0900

    env: add shellscript to install tools written in golang

---
+++ install_go.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Utility
+go get -u github.com/peco/peco/cmd/peco
+go get -u github.com/motemen/ghq
+
+# golang Development
+go get -u github.com/golang/lint/golint
+go get -u github.com/jstemmer/gotags
+go get -u github.com/k0kubun/pp
+go get -u github.com/motemen/gore
+go get -u github.com/nsf/gocode
+go get -u github.com/rogpeppe/godef
+go get -u golang.org/x/tools/cmd/cover
+go get -u golang.org/x/tools/cmd/godoc
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/vet
