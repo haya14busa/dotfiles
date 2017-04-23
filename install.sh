f3802e6bdd924357522ac868fc0aa3d3882f41bd
haya14busa <hayabusa1419@gmail.com>
2017-04-23 02:46:23 +0000

    tweak install scripts

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 ln -s ~/dotfiles/.gitconfig .gitconfig
 ln -s ~/dotfiles/.gitignore .gitignore
-ln -s ~/dotfiles/.gvimrc .gvimrc
-ln -s ~/dotfiles/.inputrc .inputrc
-ln -s ~/dotfiles/.pythonstartup .pythonstartup
 ln -s ~/dotfiles/.tmux.conf .tmux.conf
-ln -s ~/dotfiles/.vimrc .vimrc
 ln -s ~/dotfiles/.zshrc .zshrc
+ln -s ~/dotfiles/.zprofile .zprofile
 ln -s ~/dotfiles/.ctags .ctags
-ln -s ~/dotfiles/.git_commit_msg.txt .git_commit_msg.txt
 ln -s ~/dotfiles/.sshrc .sshrc
 ln -s ~/dotfiles/.sshrc.d/ .sshrc.d
--- install_go.sh
+++ install_go.sh
@@ -6,12 +6,8 @@ go get -u github.com/motemen/ghq
 
 # golang Development
 go get -u github.com/golang/lint/golint
-go get -u github.com/jstemmer/gotags
-go get -u github.com/k0kubun/pp
 go get -u github.com/motemen/gore
 go get -u github.com/nsf/gocode
 go get -u github.com/rogpeppe/godef
-go get -u golang.org/x/tools/cmd/cover
 go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/vet
